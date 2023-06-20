c1624e35696f18a17d80942b48d9f66f11ee455b
Filipe Bacof <filipebacof@gmail.com>
2023-06-19 21:49:16 -0300

    removed some projects

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,15 +3,10 @@ import { FaGithub, FaYoutube } from 'react-icons/fa'
 import { RiNavigationLine } from 'react-icons/ri'
 
 import chatGPT from '../img/projects/chatgpt.png'
-import doom from '../img/projects/Doom.jpg'
 import netflix from '../img/projects/Netflix.png'
-import nlwCopa from '../img/projects/NLW-Copa.png'
 import nlwEsports from '../img/projects/NLW-Esports.png'
 import nlwSpacetime from '../img/projects/NLW-Spacetime.png'
 import nodejs from '../img/projects/nodeJS.png'
-import pokemon from '../img/projects/pokebola-display-926x926.png'
-import probabilidade from '../img/projects/probabilidade.jpg'
-import resize from '../img/projects/resize.jpg'
 import tmdb from '../img/projects/tmdb.jpeg'
 
 const ProjectsContainer = () => {
@@ -62,7 +57,7 @@ const ProjectsContainer = () => {
           <div className='project-item'>
             <div className='logo-title'>
               <img src={nodejs} alt="Icon Node.JS" />
-              <h3>API Node.JS - SmartClasses DB</h3>
+              <h3>API Node.JS - Desafio Técnico</h3>
             </div>
             <p>
               Projeto feito como teste técnico no processo de seleção para a empresa IMM Digital.<br/>
@@ -126,77 +121,7 @@ const ProjectsContainer = () => {
             </div>
           </div>
 
-          <div className='project-item'>
-            <div className='logo-title'>
-              <img src={nlwCopa} alt="Icon NLW Copa" />
-              <h3>NLW Copa - Rocketseat</h3>
-            </div>
-            <p>
-              Evento que aconteceu em Outubro de 2022 promovido pela Rocketseat;<br/>
-              Durante esse evento, tive a oportunidade de desenvolver várias ideias que aumentaram a complexidade do projeto. Como não havia uma API disponível com os resultados dos jogos, fiz todas as atualizações manualmente, o que tornou a experiência ainda mais desafiadora;<br/>
-              Acompanhar esse evento gigante e desenvolver minhas habilidades foi uma experiência divertida, isso me motiva a retomar esse projeto durante a próxima copa, mas desta vez utilizando linguagens mais modernas e transformando-o em uma aplicação mobile em vez de apenas um site. Isso me permitirá explorar recursos avançados e fornecer uma experiência ainda melhor aos usuários. 
-            </p>
-            <div className='links'>
-              <a href="https://filipe-bacof.github.io/Calendario-Jogos-Copa/" target="_blank" className="btn-project-deploy"><RiNavigationLine />&nbsp;&nbsp;Ver Deploy</a>
-              <a href="https://github.com/Filipe-Bacof/Calendario-Jogos-Copa" target="_blank" className="btn-project-github"><FaGithub />&nbsp;&nbsp;Ver Repositório</a>
-            </div>
-          </div>
 
-          <div className='project-item'>
-            <div className='logo-title'>
-              <img src={pokemon} alt="Icon Pokebola" />
-              <h3>Pokédex</h3>
-            </div>
-            <p>
-              Projeto utilizando HTML, CSS e JavaScript utilizando a PokéAPI.<br/>
-              A PokéAPI oferece acesso a uma ampla gama de dados, incluindo informações detalhadas sobre cada Pokémon, como nome, habilidades, tipos, estatísticas, evoluções, sprites e muito mais. Ela também fornece informações sobre movimentos, itens, jogos e localizações encontradas nos jogos Pokémon.
-            </p>
-            <div className='links'>
-              <a href="https://filipe-bacof.github.io/Pokedex/" target="_blank" className="btn-project-deploy"><RiNavigationLine />&nbsp;&nbsp;Ver Deploy</a>
-              <a href="https://github.com/Filipe-Bacof/Pokedex" target="_blank" className="btn-project-github"><FaGithub />&nbsp;&nbsp;Ver Repositório</a>
-            </div>
-          </div>
-
-          <div className='project-item'>
-            <div className='logo-title'>
-              <img src={resize} alt="Icon Resize" />
-              <h3>Aprenda Media Queries do CSS3</h3>
-            </div>
-            <p>
-              Criei este repositório no GitHub para fornecer um exemplo mais claro do conceito de Media Queries do CSS3. As Media Queries são um recurso poderoso que permite adicionar responsividade aos sites, permitindo que sejam visualizados e utilizados em diferentes tamanhos de tela, como em dispositivos móveis, tablets e desktops;<br/>
-              A intenção é ajudar os desenvolvedores a compreenderem e dominarem o uso das Media Queries, permitindo que criem sites adaptáveis e amigáveis para diferentes contextos de visualização. Sinta-se à vontade para explorar o repositório, cloná-lo e utilizar o código como base para seus próprios projetos. Espero que esse recurso seja útil para você e contribua para o seu aprendizado sobre responsividade no desenvolvimento web.
-            </p>
-            <div className='links'>
-              <a href="https://github.com/Filipe-Bacof/Media-Queries-CSS" target="_blank" className="btn"><FaGithub />&nbsp;&nbsp;Ver este Projeto</a>
-            </div>
-          </div>
-
-          <div className='project-item'>
-            <div className='logo-title'>
-              <img src={probabilidade} alt="Icon Probabilidade" />
-              <h3>Monty Hall - Porta Premiada</h3>
-            </div>
-            <p>
-              Desenvolvi este projeto utilizando Next.js e Tailwind CSS, com o objetivo de proporcionar uma explicação sobre o enigma estatístico e paradoxal de Monty Hall.
-            </p>
-            <div className='links'>
-              <a href="https://porta-premiada-curso-cod3r.vercel.app/" target="_blank" className="btn-project-deploy"><RiNavigationLine />&nbsp;&nbsp;Ver Deploy</a>
-              <a href="https://github.com/Filipe-Bacof/porta-premiada" target="_blank" className="btn-project-github"><FaGithub />&nbsp;&nbsp;Ver Repositório</a>
-            </div>
-          </div>
-
-          <div className='project-item'>
-            <div className='logo-title'>
-              <img src={doom} alt="Icon Doom" />
-              <h3>Doom Fire - Filipe Deschamps</h3>
-            </div>
-            <p>Fazer esse projeto junto com o Filipe Deschamps é o que pode fazer você se apaixonar por programação, tive diversas idéias que estão registradas para treinar melhor minhas habilidades de desenvolvimento front-end.</p>
-            <div className='links'>
-              <a href="https://filipe-bacof.github.io/Domm-Fire-Deschamps/projeto%20limpo/index.html" target="_blank" className="btn-project-deploy"><RiNavigationLine />&nbsp;&nbsp;Ver Deploy</a>
-              <a href="https://github.com/Filipe-Bacof/Domm-Fire-Deschamps" target="_blank" className="btn-project-middle"><FaGithub />&nbsp;&nbsp;Ver Repositório</a>
-              <a href="https://www.youtube.com/watch?v=HCjDjsHPOco" target="_blank" className="btn-project-github"><FaYoutube />&nbsp;&nbsp;Filipe Deschamps</a>
-            </div>
-          </div>
         </div>
     </section>
   )
